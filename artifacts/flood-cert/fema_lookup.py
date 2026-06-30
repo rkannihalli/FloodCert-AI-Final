80fbeee6af4e8dd3a61a8ee9ad4fa5d70f0ebe73
Rishu Kannihalli <rishu.kannihalli@gmail.com>
2026-06-30 21:00:54 +0000

    fix: switch flood zone query from Esri Living Atlas to FEMA NFHL Layer 28 for accuracy

--- artifacts/flood-cert/fema_lookup.py
+++ artifacts/flood-cert/fema_lookup.py
@@ -23,7 +23,7 @@ _STREET_TYPE_RE = re.compile(
     re.IGNORECASE,
 )
 
-ESRI_FLOOD_ZONE_URL = (
+ESRI_FLOOD_ZONE_URL = "https://hazards.fema.gov/arcgis/rest/services/public/NFHL/MapServer/28/query"
     "https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services"
     "/USA_Flood_Hazard_Reduced_Set_gdb/FeatureServer/0/query"
 )
